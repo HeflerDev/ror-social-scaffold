e0e9b82ec3c560650897298ca6174c863a659e75
HeflerDev <hheflerdevelopment@gmail.com>
2020-04-28 17:49:58 -0300

    Basic Testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
+RSpec.describe User, type: :model do
+  subject {
+         described_class.new(password: "some_password",
+                             email: "john@doe.com",
+                             name: "john"
+         )
+  }
   it "is valid with valid attributes" do
-    expect(User.new(email:'a@sdf',name:'asdf', password:'asdfgh')).to be_valid
+    expect(subject).to be_valid
   end
   it "is not valid without a email" do
-      expect(User.new(name:'asdf', password:'asdfgh')).to be_invalid
+      subject.email = nil
+      expect(subject).to be_invalid
   end
 
   it "is not valid without a name" do
-    expect(User.new(password:'asdfgh',email:'a@sdf')).to be_invalid
+    subject.name = nil
+    expect(subject).to be_invalid
   end
 
   it "is not valid without a password" do
-    expect(User.new(email:'a@sdf', name:'asdf')).to be_invalid
+    subject.password = nil
+    expect(subject).to be_invalid
   end
 end
